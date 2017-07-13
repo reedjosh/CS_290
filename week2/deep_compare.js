76676700a98199c212ac5fc91e25aceeae63d438
reedjosh <reedjosh@oregonstate.edu>
2017-07-12 18:28:03 -0700

    Finished deep compare function.

--- week2/deep_compare.js
+++ week2/deep_compare.js
@@ -1,27 +1,34 @@
-// Your code here.
-function deepEqual(obj1, obj2){
-    if (obj1 == null || obj2 == null)
-        if (obj1 == null && obj2 == null)
-            return true;
-        else return false;
-    if (typeof(obj1) == typeof(obj2)){
-        if (typeof(obj1) === 'object') // Do deep compare...
-            for (var item in obj1) {
-                if (item not in obj2) return false;
-            } 
-            return true;
-        if (obj1 === obj2)
-            return true;
-        else
-            return false;
-        }
-    else // Type mismatch
-        return false;
+// Joshua Reed
+// CS 290 Web Development
+// Oregon State EECS
+//
+// This file compares two JSON objects.
+//
+// I happened upon a great answer, and while I didn't copy the code, I did take
+// the key sorting idea from here:
+// https://stackoverflow.com/questions/13142968/deep-comparison-of-objects-arrays 
 
+function deepEqual(item1, item2){
+
+    // Check whether both items are objects but not null.
+    // If not, then just return the direct comparison of the two.
+    if ((typeof(item1) == "object" && typeof(item2) == "object") && (item2 != null && item1 != null)){
+        // Check that these have the same number of keys
+        var keys1 = Object.keys(item1);
+        var keys2 = Object.keys(item2);
+        if (keys1.length != keys2.length) return false;
+
+        // Check that the keys match, and that their content matches.
+        // Do this recursively all the way down. 
+        for (var key in item1){
+            if (!(key in item2) || !deepEqual(item1[key], item2[key])) return false;
+            }
+        return true;
+        }
+    else return (item1 === item2); // Directly compare base elements.
 }
 
 var obj = {here: {is: "an"}, object: 2};
-
 console.log(deepEqual(obj, obj));
 // → true
 
